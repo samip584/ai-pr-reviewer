e3a1759c7cec7d69d99c849c14da713bb2d49efa
SamipSthaLF <samip_shrestha@lftechnology.com>
2023-03-26 10:00:00 +0545

    feat: add model selection and basic prompt system

--- rwgpt.py
+++ rwgpt.py
@@ -21,6 +21,19 @@ except Exception:
     sys.exit(2)
 
 
+def resolve_model(model: str) -> str:
+    aliases = {
+        "gpt4": "gpt-4",
+        "gpt-3.5": "gpt-3.5-turbo",
+        "gpt-3.5-turbo": "gpt-3.5-turbo",
+    }
+    return aliases.get(model.lower(), model)
+
+
+def build_user_prompt(diff_text: str) -> str:
+    return f"Review this git diff and provide feedback:\n\n```diff\n{diff_text}\n```"
+
+
 def read_input(args: argparse.Namespace) -> str:
     if args.input is not None:
         return args.input
@@ -42,13 +55,29 @@ def main() -> None:
         help="The diff to review. Example: --input \"$(git show -U4)\"",
     )
     parser.add_argument("--input-file", help="Path to a file containing the diff input")
+    parser.add_argument(
+        "-m",
+        "--model",
+        default="gpt-4",
+        help="Model name (e.g., gpt4, gpt-4, gpt-3.5-turbo)",
+    )
+    parser.add_argument(
+        "--max", dest="max_tokens", type=int, default=900, help="Max output tokens"
+    )
+    parser.add_argument("-t", "--temperature", type=float, default=0.2)
     parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument(
+        "--no-alias",
+        action="store_true",
+        help="Do not map model aliases",
+    )
     args = parser.parse_args()
 
     if not os.getenv("OPENAI_API_KEY"):
         print("Error: OPENAI_API_KEY is not set in the environment.", file=sys.stderr)
         sys.exit(2)
 
+    model = args.model if args.no_alias else resolve_model(args.model)
     diff_text = read_input(args)
 
     if not diff_text.strip():
@@ -56,9 +85,15 @@ def main() -> None:
         sys.exit(2)
 
     if args.verbose:
+        print(
+            f"[rwgpt] model={model} max_tokens={args.max_tokens} temp={args.temperature}",
+            file=sys.stderr,
+        )
         print(f"[rwgpt] input chars={len(diff_text)}", file=sys.stderr)
 
-    print("Basic CLI structure ready!")
+    user_prompt = build_user_prompt(diff_text)
+    print(f"Ready to review with {model}!")
+    print(f"Prompt preview: {user_prompt[:100]}...")
 
 
 if __name__ == "__main__":
